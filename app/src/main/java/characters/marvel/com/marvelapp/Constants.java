cb43969aa5fd5b38d6379a0cec7600fed4c69794
Ruairi McGuigan <rmcg2@allstate.com>
2017-02-04 12:04:11 +0000

    init commit

---
+++ app/src/main/java/characters/marvel/com/marvelapp/Constants.java
@@ -0,0 +1,11 @@
+package characters.marvel.com.marvelapp;
+
+/**
+ * Created by rmcg2 on 04/02/2017.
+ */
+
+public class Constants {
+
+    public static final String PUBLIC_KEY = "b7ce88aa539fedec040a84f234de0822";
+    public static final String PRIVATE_KEY = "ff9473f4226204cff09886ff28e445944d50cd75";
+}
